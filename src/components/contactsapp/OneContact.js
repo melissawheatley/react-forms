4925e22350565b252ed08b0e2b5e8cd0d6f2d1f1
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-05-30 13:58:45 -0500

    add edit form to OneContact view

--- src/components/contactsapp/OneContact.js
+++ src/components/contactsapp/OneContact.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Grid, Image, Icon, Rating, Divider, Button } from 'semantic-ui-react';
+import { Grid, Image, Icon, Divider, Button } from 'semantic-ui-react';
 import './main.css';
 import { Link } from 'react-router-dom';
+import EditForm from './EditForm';
 
 export default class OneContact extends Component{
 
@@ -25,6 +26,47 @@ export default class OneContact extends Component{
             // console.log('contact from getContactDB', this.state.contact);
         })
     }
+
+    editContact = () => {
+        this.setState({
+            edit: true
+        })
+    }
+
+    saveUpdate = (userObject) =>{
+        return fetch(`http://localhost:4000/contacts/${this.props.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userObject)
+        }).then((data)=>{
+            return data.json();
+        }).then((result)=>{
+            this.setState({
+                contact: result, //you want to set this state so that the update will re-render on it's own without refresh.
+                edit: false
+            })
+        })
+    }
+
+    cancelUpdate = () => {
+        this.setState({
+            edit: false
+        })
+    }
+
+    openEditForm = () =>{
+        if(this.state.edit){
+            return(
+                <div>
+                    <Divider section />
+                    <EditForm title='Edit This Contact' person={this.state.contact} saveUpdate={this.saveUpdate} cancelUpdate={this.cancelUpdate} />
+                </div>
+            )
+        }
+    }
+
 
 
     render(){
@@ -56,8 +98,8 @@ export default class OneContact extends Component{
                     <p>{this.state.contact.phone}</p>
                     <p><a href={`mailto:${this.state.contact.email}`}>{this.state.contact.email}</a></p>
                     <p>{this.state.contact.notes}</p>
-                    <Rating maxRating={5} defaultRating={this.state.contact.rating} icon='star' size='massive' />
-                    {/* {this.openForm()} */}
+                    {/* <Rating maxRating={5} defaultRating={this.state.contact.rating} icon='star' size='massive' /> */}
+                    {this.openEditForm()}
                 </Grid.Column>
             </Grid>
             
